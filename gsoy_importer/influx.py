3e17e0233dfbbcfcec950a3017438f48974dc521
Dean Dalianis <dean.dalianis@cern.ch>
2023-06-26 17:06:11 +0300

    bugfix

--- gsoy_importer/influx.py
+++ gsoy_importer/influx.py
@@ -3,7 +3,6 @@ import time
 from config import HOST, PORT, USERNAME, PASSWORD, DB_NAME
 from influxdb import InfluxDBClient
 from logging_config import logger
-from util import ms_to_timestamp
 
 client = InfluxDBClient(
     host=HOST,
@@ -29,34 +28,6 @@ def write_points_to_db(points):
         client.write_points(points)
     except Exception as e:
         logger.error(f'Failed to write data, {e}')
-
-
-def fetch_gsom_data_from_db(country, datatype):
-    """
-    Fetch all climate data for a specified country and datatype from DB.
-
-    :param dict country: The country to fetch the climate data for.
-    :param str datatype: The datatype corresponding to a measurement in DB.
-    :return: A list of records, where each record is a dictionary with 'date' and 'value'.
-    :rtype: list[dict] or None
-    """
-
-    query = f"SELECT * FROM \"{datatype}\" WHERE \"country_id\" = '{country['iso']}'"
-    result = client.query(query, epoch='ms')
-
-    if result:
-        records = []
-        for point in result.get_points():
-            record = {
-                'date': ms_to_timestamp(point['time']),
-                'value': point['value']
-            }
-            records.append(record)
-        return records
-    else:
-        logger.warning(f"No data found in DB for country: {country['name']} and datatype: {datatype}")
-        return None
-
 
 def wait_for_db():
     """
